97ac35dbfb86b79aa52ba6cde3b425fc3613caad
caugustoam <caugustoam@ENGNB000961>
2022-06-26 19:55:58 -0300

    Adicionar arquivos de projeto.

---
+++ ITrade.cs
@@ -0,0 +1,8 @@
+﻿namespace Trades.Properties
+{
+    public interface ITrade
+    {
+        double Value { get; }
+        string ClientSector { get; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using Trades.Properties;
+
+public class Program : ITrade
+{
+    public double Value => throw new NotImplementedException();
+
+    public string ClientSector => throw new NotImplementedException();
+
+    public static void Main()
+    {
+        Categories categoria = new Categories();
+        List<ITrade> s = new List<ITrade>();
+
+
+        var sx = categoria.Trades(s);
+    }
+}
+public class Categories : ITrade
+{
+
+    public List<ITrade> portfolio;
+    public List<string> tradeCategories;
+
+    public double Value => throw new NotImplementedException();
+
+    public string ClientSector => throw new NotImplementedException();
+
+    public List<string>Trades (List<ITrade> portfolio) 
+    {
+        var category = "";
+
+        foreach (var element in portfolio)
+        {
+            if ((element.Value < 10000000) & element.ClientSector.Equals("Public"))
+            {
+                category = "LOWRISK";
+            }
+            else if ((element.Value > 10000000) & element.ClientSector.Equals("Public"))
+            {
+                category = "MEDIUMRISK";
+            }
+            else if ((element.Value > 10000000) & element.ClientSector.Equals("Private"))
+            {
+                category = "HIGHRISK";
+            }
+                        
+            tradeCategories.Add(category);
+        }
+
+        return tradeCategories;
+    }
+}
